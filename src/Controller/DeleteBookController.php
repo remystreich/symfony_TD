ef8ae76a66e87ac71e96752b4016b51f8fcbd0d8
remystreich <remystreich@gmail.com>
2024-05-17 19:04:09 +0000

    suppression livre

--- src/Controller/DeleteBookController.php
+++ src/Controller/DeleteBookController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DeleteBookController extends AbstractController
 {
-    #[Route('/delete/book', name: 'app_delete_book')]
-    public function index(): Response
+    #[Route('/books/delete/{id}', name: 'book_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function __invoke(Book $book, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('delete_book/index.html.twig', [
-            'controller_name' => 'DeleteBookController',
-        ]);
+        $entityManager->remove($book);
+        $entityManager->flush();
+        return $this->redirectToRoute('book_index');
     }
 }
